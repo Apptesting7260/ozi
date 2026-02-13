bb2131dccfc0b469db7f10a2d06b60f2beeddfdd
Apptesting7260 <testdemo7260@gmail.com>
2026-02-13 17:05:19 +0530

    ios changes

--- lib/app/modules/user/profile/edit profile/view/EditProfileScreen.dart
+++ lib/app/modules/user/profile/edit profile/view/EditProfileScreen.dart
@@ -142,14 +142,15 @@ class EditProfileScreen extends StatelessWidget {
               },
             ),
 
-            ListTile(
-              leading: Icon(Icons.camera_alt, color: AppColors.primary),
-              title: const Text("Camera"),
-              onTap: () {
-                Navigator.pop(context);
-                provider.pickCamera();
-              },
-            ),
+            // ListTile(
+            //   leading: Icon(Icons.camera_alt, color: AppColors.primary),
+            //   title: const Text("Camera"),
+            //   onTap: () {
+            //     Navigator.pop(context);
+            //     provider.pickCamera();
+            //   },
+            // ),
+            // ),
 
             hBox(10),
           ],
